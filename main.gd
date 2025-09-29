2a5e178fe35446bbf0cb3b4390c2a92614248d3e
Anuj Dubey <dubey2175@gmail.com>
2025-09-29 17:35:27 +0530

    Ball spawning added

--- main.gd
+++ main.gd
@@ -1,11 +1,49 @@
 extends Node2D
 
+@onready var spawner = $BallSpawner
+var ball_scenes = [
+	preload("res://ball0.tscn"),
+	preload("res://ball1.tscn"),
+	preload("res://ball2.tscn"),
+	preload("res://ball3.tscn"),
+	preload("res://ball4.tscn"),
+	preload("res://ball5.tscn")
+]
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var preview_ball: Node2D = null
+var can_move = true
 
+func _ready():
+	spawn_preview()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Create a preview ball at spawner
+func spawn_preview():
+	var BallScene = ball_scenes[randi() % ball_scenes.size()]
+	preview_ball = BallScene.instantiate()
+	preview_ball.position = spawner.position
+	preview_ball.freeze = true  # stops all physics
+	add_child(preview_ball)
+
+func _process(delta):
+	if preview_ball and can_move:
+		if Input.is_action_pressed("ui_left"):
+			preview_ball.position.x -= 200 * delta
+		if Input.is_action_pressed("ui_right"):
+			preview_ball.position.x += 200 * delta
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		drop_ball()
+
+	if event is InputEventScreenTouch and event.pressed:
+		drop_ball()
+
+func drop_ball():
+	if preview_ball:
+		preview_ball.freeze = false
+		preview_ball = null
+		can_move = false
+		# Wait a short delay, then allow next ball
+		await get_tree().create_timer(0.5).timeout
+		can_move = true
+		spawn_preview()
